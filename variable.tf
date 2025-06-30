6512a23988f3ad35408f4c96641b53cf2f188f56
Bluet47 <treasure.k.muriithi@gmail.com>
2025-06-30 13:30:08 +0100

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,10 @@
+variable "project_id" {
+  description = "test-flask-api-460611"
+  type        = string
+}
+
+variable "region" {
+  description = "Region to deploy the resources"
+  type        = string
+  default     = "us-central1"
+}
