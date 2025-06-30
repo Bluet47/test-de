d26df0a0ee81bd18d2b2414443ceb9b0808184cc
Bluet47 <treasure.k.muriithi@gmail.com>
2025-06-30 13:41:02 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,16 @@
 provider "google" {
   project = var.project_id
   region  = var.region
+  zone    = var.zone
 }
 
+# VPC
 resource "google_compute_network" "custom_vpc" {
   name                    = "custom-vpc"
   auto_create_subnetworks = false
 }
 
+# Subnet
 resource "google_compute_subnetwork" "custom_subnet" {
   name          = "custom-subnet"
   ip_cidr_range = "10.0.0.0/24"
@@ -15,16 +18,58 @@ resource "google_compute_subnetwork" "custom_subnet" {
   network       = google_compute_network.custom_vpc.id
 }
 
+# Firewall rule to allow SSH
+resource "google_compute_firewall" "allow_ssh" {
+  name    = "allow-ssh"
+  network = google_compute_network.custom_vpc.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
+
+# Cloud Router
 resource "google_compute_router" "nat_router" {
   name    = "nat-router"
   region  = var.region
   network = google_compute_network.custom_vpc.id
 }
 
+# Cloud NAT
 resource "google_compute_router_nat" "nat_config" {
   name                               = "nat-config"
   router                             = google_compute_router.nat_router.name
   region                             = var.region
   nat_ip_allocate_option             = "AUTO_ONLY"
   source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+}
+
+# VM instance
+resource "google_compute_instance" "vm_instance" {
+  name         = "my-vm"
+  machine_type = "e2-medium"
+  zone         = var.zone
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-12"
+    }
+  }
+
+  network_interface {
+    subnetwork = google_compute_subnetwork.custom_subnet.id
+
+    access_config {
+      # Required to give VM a public IP for internet access
+    }
+  }
+
+  metadata = {
+    ssh-keys = "your-username:${file("~/.ssh/id_rsa.pub")}"
+  }
+
+  tags = ["ssh"]
 }
